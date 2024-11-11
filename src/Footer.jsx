bca35f662d9a0b8cd8227f45c61c187081078f8b
Webymind <webymind.mauritius@gmail.com>
2024-11-11 14:53:46 +0400

    main

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
     className="text-center py-3 shadow-lg hoe mt-4"
     style={{
       backgroundColor: "rgba(255, 255, 255, 0.85)", // Translucent white background
-      backdropFilter: "blur(5px)", // Blur effect for consistency
+      backdropFilter: "blur(1px)", // Blur effect for consistency
       borderRadius: "10px", // Rounded corners for card-like appearance
       // width: "fit-content", // Make width compact
       // margin: "0 auto", // Center align horizontally
@@ -15,7 +15,7 @@ const Footer = () => (
       justifyContent: "center",
     }}
   >
-    &copy; 2024{" "}
+    &copy;{" "}
     <a
       style={{
         color: "#76c014",
